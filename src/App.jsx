24bf7fa597907fb5a2123810978aedb951c9f0d5
unknown <milan_cirkovic@yahoo.com>
2020-12-12 21:21:03 +0100

    create current data container with children

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,22 @@ function App() {
             value={query}
           />
         </div>
+        <div className="current__weather_image">
+          <p>svg wather</p>
+        </div>
+        <div className="current_temperature">
+          <h1>21 C</h1>
+          <h5>feels like 20 C</h5>
+        </div>
+        <div className="date_day_time">
+          <h2>01.01.2020</h2>
+          <h2>Sunday</h2>
+          <h3>time 12:00</h3>
+        </div>
+        <div className="current_city">
+          <span>city img</span>
+          <h2>NEW YORK, USA</h2>
+        </div>
       </div>
       <div className="additional__data__container">
         <h2>additional container</h2>
